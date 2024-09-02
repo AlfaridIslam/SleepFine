1a08e0846b33237b86c3956ab7af7df38c1f5b11
Pvenkateshp11 <patlavathvenkatesh2001@gmail.com>
2024-09-02 15:42:05 +0530

    second commit

--- client/src/components/Whatsapp/Whatsapp.jsx
+++ client/src/components/Whatsapp/Whatsapp.jsx
@@ -1,15 +1,14 @@
+/* eslint-disable-next-line no-unused-vars */
 import React from 'react';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Whatsapp.css";
 
-
 const Whatsapp = () => {
   return (
     <div>
-       <a href="https://wa.me/9346023771" className="whatsapp_float" target="_blank" rel="noopener noreferrer">
+       <a href="https://wa.me/8898916144" className="whatsapp_float" target="_blank" rel="noopener noreferrer">
         <i className="fa fa-whatsapp whatsapp-icon"></i>
       </a> 
-       
     </div>
   )
 }
