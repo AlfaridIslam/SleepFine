62772632c7868ccbc2c07ae2f12913ebbea9473d
alfarid <alfaridislam@gmail.com>
2024-09-03 16:53:15 +0530

    third commit

--- client/src/components/DynamicImages/DynamicImages.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DynamicImages = () => {
-  return (
-    <div>DynamicImages</div>
-  )
-}
-
-export default DynamicImages
--- client/src/components/ProductList/ProductList.jsx
+++ client/src/components/ProductList/ProductList.jsx
@@ -1,25 +1,25 @@
-// ProductList.jsx
-import React from "react";
+// // ProductList.jsx
+// import React from "react";
 
-const ProductList = ({ subcategories }) => {
-  return (
-    <div>
-      {subcategories && subcategories.length > 0 && (
-        <ul className="subcategories-list">
-          {subcategories.map((subcategory, index) => (
-            <li key={index}>
-              <img
-                src={subcategory.image}
-                alt={`Subcategory ${index + 1}`}
-                className="subcategory-image"
-              />
-              <p>{subcategory.name}</p>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-};
+// const ProductList = ({ subcategories }) => {
+//   return (
+//     <div>
+//       {subcategories && subcategories.length > 0 && (
+//         <ul className="subcategories-list">
+//           {subcategories.map((subcategory, index) => (
+//             <li key={index}>
+//               <img
+//                 src={subcategory.image}
+//                 alt={`Subcategory ${index + 1}`}
+//                 className="subcategory-image"
+//               />
+//               <p>{subcategory.name}</p>
+//             </li>
+//           ))}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// };
 
-export default ProductList;
+// export default ProductList;
