43c73e49b0227db5bd9a5c29dff6cbc11aac9246
alfarid <alfaridislam@gmail.com>
2024-09-04 14:26:55 +0530

    whatsaap/gotoTop/Resp

--- client/src/components/GoToTop/GoToTop.jsx
+++ client/src/components/GoToTop/GoToTop.jsx
@@ -1,42 +1,45 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const GoToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
 
-    const [isVisible, setIsVisible] = useState(false);
+  const top = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
 
-    const top = () => {
-        window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-    };
+  const listenToScroll = () => {
+    const heightToHidden = 200;
+    const winScroll =
+      document.documentElement.scrollTop || document.body.scrollTop;
 
-    const listenToScroll = () => {
-        const heightToHidden = 200;
-        const winScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if (winScroll > heightToHidden) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
 
-        if (winScroll > heightToHidden) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
-    };
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, []);
 
-    useEffect(() => {
-        window.addEventListener('scroll', listenToScroll);
-        return () => window.removeEventListener('scroll', listenToScroll);
-    }, []);
-
-    return (
-        <>
-            {isVisible && (
-                <div>
-                    <button onClick={top} className='fixed bottom-4 right-4 z-50 text-white bg-violet-600 border-cyan-700 rounded-full p-5 w-8 h-8 flex items-center justify-center shadow-black'>
-                        <FontAwesomeIcon icon={faArrowUp}  className='animate-pulse'/>
-                    </button>
-                </div>
-            )}
-        </>
-    );
-}
+  return (
+    <>
+      {isVisible && (
+        <div>
+          <button
+            onClick={top}
+            className="fixed bottom-4 right-1 z-50 text-white bg-blue-600 border-cyan-700 rounded-full p-5 w-8 h-8 flex items-center justify-center shadow-black"
+          >
+            <FontAwesomeIcon icon={faArrowUp} className="animate-pulse" />
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
 
 export default GoToTop;
--- client/src/components/Whatsapp/Whatsapp.jsx
+++ client/src/components/Whatsapp/Whatsapp.jsx
@@ -1,16 +1,27 @@
 /* eslint-disable-next-line no-unused-vars */
-import React from 'react';
+import React from "react";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Whatsapp.css";
 
 const Whatsapp = () => {
   return (
-    <div>
-       <a href="https://wa.me/8898916144" className="whatsapp_float" target="_blank" rel="noopener noreferrer">
-        <i className="fa fa-whatsapp whatsapp-icon"></i>
-      </a> 
+    <div className="whatsapp-container">
+      <div className="whatsapp_float">
+        <a
+          href="https://wa.me/8898916144"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa fa-whatsapp whatsapp-icon mb-[10px]"></i>
+          <div className="whatsapp-text flex justify-center gap-1 mt-4">
+            <p>Chat</p>
+            <p>with</p>
+            <p>us</p>
+          </div>
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Whatsapp
+export default Whatsapp;
