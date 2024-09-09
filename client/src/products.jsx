a200b83205599f6a5d2cca0a6b4eb138e63bbe0e
sreenivas782 <sreenivasathi333@gmail.com>
2024-09-09 18:28:34 +0530

    sreenivas@ 090/09-products.jsx   venky-product-types

--- client/src/products.jsx
+++ client/src/products.jsx
@@ -9,17 +9,25 @@ import {
 } from "./assets/index"; // Adjust the path according to your folder structure
 
 const products = [
+
+  //-------------------Mattres-------------
   {
     id: 1,
     name: "Mattress",
     image: Sofa,
+    //----------Mattress-SUB-----------------
     subcategories: [
+
+      //----------Orthopedic Bonded Collection--------
       {
         id: 101,
         name: "Orthopedic Bonded Collection",
         image: LeatherSofa,
+
         subitems: [
-          // Adding subitems under Orthopedic Bonded Collection
+
+           //----------Orthopedic Bonded Collection-SUB--------
+          
           {
             id: 1011,
             name: "Orthomed",
@@ -47,20 +55,117 @@ const products = [
           },
         ],
       },
+
+      // ------------------------Ortho Bonnell Spring Collection----------------------------
       {
         id: 102,
         name: "Ortho Bonnell Spring Collection",
         image: FabricSofa,
+         // ------------------------Ortho Bonnell Spring Collection-SUB----------------------------
+        subitems: [
+          // Adding subitems under Orthopedic Bonded Collection
+          {
+            id: 1021,
+            name: "6inch Silver Corown",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1022,
+            name: "6inch Oxford",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1023,
+            name: "8inch Love Land",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1024,
+            name: "8inch Love Land Pillow Top",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1025,
+            name: "8inch Romantic Euroton",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1026,
+            name: "Aloe-Vera With Latex",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1027,
+            name: "Aloe-Vera With Memory",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+        ],
+       
       },
+
+         //--------------Pocketed Spring Collection----------------------------------
+
       {
         id: 103,
         name: "Pocketed Spring Collection",
         image: FabricSofa,
+
+        //--------------Pocketed Spring Collection---SUB----------------------------------
+        subitems: [
+          // Adding subitems under Orthopedic Bonded Collection
+          {
+            id: 1031,
+            name: "Inspiration",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1032,
+            name: "6inch Eternity-Euroton",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1033,
+            name: "Aloe-Vera With Latex",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1034,
+            name: "Aloe-Vera With Memory",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+        
+        ],
       },
+      //-----------------HR PU Foam Collection-----------------------
       {
         id: 104,
         name: "HR PU Foam Collection",
         image: FabricSofa,
+        //-----------------HR PU Foam Collection--SUB-----------------------
+        subitems: [
+          // Adding subitems under Orthopedic Bonded Collection
+          {
+            id: 1041,
+            name: "Gravity",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1042,
+            name: "Space",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1043,
+            name: "Plush",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+          {
+            id: 1044,
+            name: "Techniko",
+            image: LeatherSofa, // Using existing image as dummy
+          },
+        
+        ],
       },
     ],
   },
