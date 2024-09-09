1e8d6d58bf86062e1751e15eea1252a70f5f779f
alfarid <alfaridislam@gmail.com>
2024-09-09 15:01:34 +0530

    exceptional quality@09/09/ responsive

--- client/src/components/Section2/Section2.jsx
+++ client/src/components/Section2/Section2.jsx
@@ -77,18 +77,26 @@ const Section2 = () => {
 
   return (
     <div className="relative">
-      <div className="flex justify-between mx-20 my-7">
-        <div className="ml-[20px]">
-          <h1 className="text-3xl font-sans">Exceptional Quality</h1>
-          <p className="font-thin">Soon-to-be staples in your rotation</p>
-        </div>
-        <div className="flex justify-between gap-2 font-thin">
-          <p className="underline">Spring Mattresses</p>
-          <p>Medical Rebonded</p>
-          <p>Super Soft</p>
+      <div className="mx-20 my-7 flex-col">
+        <div className="ml-[20px] text-center flex-col justify-center items-center ">
+          <h1
+            className="xl:text-4xl md:text-3xl lg:text-2xl sm:text-xl 
+          font-sans "
+          >
+            Exceptional Quality
+          </h1>
+          <p
+            className="xl:font-thin xl:text-[16px] xl:mt-2 md:text-[12px] 
+          md:mt-2 md:font-thin lg:text-[12px] lg:mt-2 lg:font-thin sm:font-thin sm:text-[12px] sm:mt-2"
+          >
+            Soon-to-be staples in your rotation
+          </p>
         </div>
       </div>
-      <div className="relative w-full flex justify-center items-center p-12">
+      <div
+        className="relative w-full flex justify-center items-center p-12 sm:p-4 sm:w-[810px] md:w-[810px] md:p-4 
+      lg:w-[139%] lg:p-1 xl:w-full xl:p-4"
+      >
         {!isAtStart && (
           <button
             onClick={scrollLeft}
@@ -100,7 +108,7 @@ const Section2 = () => {
         <div
           ref={scrollRef}
           onScroll={handleScroll}
-          className="flex overflow-x-auto space-x-6 hide-scrollbar"
+          className="flex gap-4 overflow-x-auto space-x-6 hide-scrollbar sm:space-x-0 sm:overflow-hidden"
           style={{ scrollBehavior: "smooth" }}
         >
           {images.map((img, index) => (
@@ -108,7 +116,7 @@ const Section2 = () => {
               key={index}
               className="relative h-[500px] flex-shrink-0"
               style={{
-                width: "30%", // Adjust this value to control the width of each image
+                width: "30%", // Default width for larger screens
               }}
               onMouseEnter={() => handleMouseEnter(index)}
               onMouseLeave={handleMouseLeave}
@@ -116,7 +124,7 @@ const Section2 = () => {
               <img
                 src={hoveredIndex === index ? hoveredImages[index] : img}
                 alt={`Image ${index + 1}`}
-                className="w-full h-full object-cover transition-all duration-300"
+                className="w-full h-full object-cover transition-all duration-300 sm:w-[100vw]" // Full width on small screens
               />
               {hoveredIndex === index && (
                 <div className="absolute bottom-0 w-full z-50 bg-black bg-opacity-50 text-white text-center py-2 opacity-90 transition-opacity cursor-pointer">
