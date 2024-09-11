4b809b1899ccb7eef33d25378a47e388e3f271b4
alfarid <alfaridislam@gmail.com>
2024-09-11 18:19:06 +0530

    responsive completed@home page@11/9

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -11,89 +11,85 @@ import {
 
 const Footer = () => {
   return (
-    <div className='Main-Sectio bg-black xl:w-full sm:w-[665px] md:w-[1082px] lg:w-[1521px] '>
-
-<div className="text-white  xl:flex xl:mt-10 xl:pt-10 xl:justify-around xl:w-full
-                 sm:mt-2  sm:grid ">
+    <div className="Main-Sectio bg-black xl:w-full sm:w-[399%] md:w-[400%] lg:w-[400%]  ">
+      <div
+        className="text-white  xl:flex xl:mt-10 xl:pt-10 xl:justify-around xl:w-full lg:pl-[8rem] lg:pr-[6relg
+                 sm:mt-2  sm:grid sm:pl-[9rem] sm:pr-[11rem] md:pl-[8rem] md:pr-[6rem] "
+      >
         <div>
-          <h1 className="text-lg font-normal xl:mt-0 sm:mt-5 xl:justify-start sm:justify-start sm:flex sm:text-xl sm:px-4">OUR PROMISE</h1>
+          <h1 className="text-lg font-normal xl:mt-0 sm:mt-5 xl:justify-start sm:justify-start sm:flex sm:text-xl sm:px-4">
+            OUR PROMISE
+          </h1>
           <ul className="font-extralight xl:mt-10 xl:text-sm sm:mt-2 sm:text-base sm:justify-center sm:flex">
             <li>
               {" "}
-              <p className='xl:w-60  sm:px-4'>
-                We at Sleep fine, are constantly pushing the  boundaries
-                of technology and innovations to 
-                craft best sleep solutions that are built 
-                around your personal comfort preferences.
-                 There is only one mattress that understands
-                 you, your body and how you sleep. 
+              <p
+                className="xl:w-60 xl:pr-[0.75rem] xl:pl-[0.25rem] lg:pl-[1rem] lg:pr-[80rem]
+               sm:pr-[13rem] sm:pl-[1rem] md:pr-[80rem] md:pl-[1rem] "
+              >
+                We at Sleep fine, are constantly pushing the boundaries of
+                technology and innovations to craft best sleep solutions that
+                are built around your personal comfort preferences. There is
+                only one mattress that understands you, your body and how you
+                sleep.
               </p>
             </li>
           </ul>
         </div>
 
-        <div className='  xl:gap-24 xl:p-0 xl:text-lg sm: flex sm:justify-between sm:px-4 '>
+        <div className="  xl:gap-24 xl:p-0 xl:text-lg sm: flex sm:justify-between sm:px-4 ">
+          <div className="xl:mt-0 sm:mt-4">
+            <h1>CATEGORIES</h1>
+            <ul className="font-extralight xl:mt-10  sm:mt-2 xl:text-sm sm:text-lg">
+              <li>Mattresses</li>
+              <li>Head Boards</li>
+              <li>Comforters</li>
+              <li>Pillows</li>
+            </ul>
+          </div>
 
-       
+          <div className="xl:mt-0 sm:mt-4">
+            <h1>SUPPORT</h1>
+            <ul className="font-extralight xl:mt-10  sm:mt-2  xl:text-sm sm:text-lg">
+              <li>Privacy Policy</li>
+              <li>Terms & Conditions</li>
+              <li>Warranty</li>
+            </ul>
+          </div>
 
-        <div className='xl:mt-0 sm:mt-4'>
-          <h1>CATEGORIES</h1>
-          <ul className="font-extralight xl:mt-10  sm:mt-2 xl:text-sm sm:text-lg">
-            <li>Mattresses</li>
-            <li>Head Boards</li>
-            <li>Comforters</li>
-            <li>Pillows</li>
-          </ul>
+          <div className="xl:mt-0 sm:mt-4">
+            <h1>CONTACT US</h1>
+            <ul className="font-extralight xl:mt-10  sm:mt-2  xl:text-sm sm:text-lg">
+              <li>E-mail: sales@sleepfineindia.com</li>
+              <li>Tel : +91-40-4040032084</li>
+            </ul>
+          </div>
         </div>
 
-        <div className='xl:mt-0 sm:mt-4'>
-          <h1>SUPPORT</h1>
-          <ul className="font-extralight xl:mt-10  sm:mt-2  xl:text-sm sm:text-lg">
-            <li>Privacy Policy</li>
-            <li>Terms & Conditions</li>
-            <li>Warranty</li>
-          </ul>
-        </div>
-
-        <div className='xl:mt-0 sm:mt-4'>
-          <h1>CONTACT US</h1>
-          <ul className="font-extralight xl:mt-10  sm:mt-2  xl:text-sm sm:text-lg">
-            <li>E-mail: sales@sleepfineindia.com</li>
-            <li>Tel : +91-40-4040032084</li>
-          </ul>
-        </div>
-
-       
-
-        </div>
-
-      
-
-<div className='sm:flex sm:justify-center'>
-        <div className='xl:mt-0 sm:mt-4 '>
-          <h1>PAYMENT METHODS</h1>
-          <ul className="font-extralight xl:mt-10 grid xl:gap-2 sm:gap-1 text-sm">
-            <li>
-              <img
-                src="src\assets\Fotter-Img\img-1.png"
-                alt=""
-                className="w-40"
-              />
-            </li>
-            <li>
-              {" "}
-              <img
-                src="src\assets\Fotter-Img\img-2.png"
-                alt=""
-                className="w-40"
-              />
-            </li>
-          </ul>
+        <div className="sm:flex sm:justify-center">
+          <div className="xl:mt-0 sm:mt-4 ">
+            <h1>PAYMENT METHODS</h1>
+            <ul className="font-extralight xl:mt-10 grid xl:gap-2 sm:gap-1 text-sm">
+              <li>
+                <img
+                  src="src\assets\Fotter-Img\img-1.png"
+                  alt=""
+                  className="w-40"
+                />
+              </li>
+              <li>
+                {" "}
+                <img
+                  src="src\assets\Fotter-Img\img-2.png"
+                  alt=""
+                  className="w-40"
+                />
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-     
-    </div>
-    <div className="text-white mt-10 sm:text-3xl xl:text-lg">
+      <div className="text-white mt-10 sm:text-3xl xl:text-lg">
         <h1 className="border-[1px] h-0 mx-14"></h1>
 
         <h1 className="justify-center items-center flex mt-10">FOLLOW US</h1>
@@ -125,9 +121,8 @@ const Footer = () => {
           Copyright.All rights reserved @ 2024
         </li>
       </div>
-
     </div>
-  )
+  );
 }
 
 export default Footer;
--- client/src/components/Updates/Updates.jsx
+++ client/src/components/Updates/Updates.jsx
@@ -7,8 +7,10 @@ import news from "../../assets/news.jpg";
 export const Updates = () => {
   return (
     <div className="xl:grid sm:grid ">
-      <div className="xl:text-center text-4xl poppins-medium xl:mt-20 xl:mb-10 xl:w-full xl:ml-0
-                      sm:mt-14  sm:ml-40  sm:text-3xl">
+      <div
+        className="xl:text-center text-4xl poppins-medium xl:mt-20 xl:mb-10 xl:w-full xl:ml-0
+                      sm:mt-14  sm:ml-40  sm:text-3xl"
+      >
         News & Updates
       </div>
 
@@ -17,18 +19,17 @@ export const Updates = () => {
       xl:items-center xl:justify-between xl:p-[135px] xl:w-[650px] xl:h-[200px] xl:ml-12 xl:relative xl:bg-slate-50 xl:mt-0
        sm:mt-10 sm:shadow sm:bg-slate-50 sm:rounded-xl sm:opacity-1 sm:border-2 sm:w-[200px] "
       >
-
-        
         <div className="image xl:border-none xl:border-2 xl:p-15 xl:absolute xl:left-2">
           <img
             src={news}
             alt="Image"
-           
             className="xl:rounded-xl xl:w-[250px] xl:h-[250px] xl:ml-0 xl:mr-0 sm:w-[180px] sm:h-[180px] sm:ml-[10px] sm:mr-[8px]  sm:rounded-xl"
           />
         </div>
-        <div className="content xl:flex-col  xl:justify-center xl:space-y-9 xl:p-10 xl:absolute xl:left-46 xl:ml-[96px] xl:mt-0
-                           sm:-mt-40 sm:ml-52 sm:w-96 sm:mb-10">
+        <div
+          className="content xl:flex-col  xl:justify-center xl:space-y-9 xl:p-10 xl:absolute xl:left-46 xl:ml-[96px] xl:mt-0
+                           sm:-mt-40 sm:ml-52 sm:w-96 sm:mb-10"
+        >
           <p className="poppins-light ">
             We're excited to announce the grand opening of our new store in
             Shahpur, Hyderabad! 🎉 Visit us to explore an exclusive range of
