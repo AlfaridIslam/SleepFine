3b673bb5152e04c0f6e57ab898f5b8c920189822
sreenivas782 <sreenivasathi333@gmail.com>
2024-10-03 11:39:19 +0530

    03/10---Sreenivas--whatsapp

--- client/src/components/Whatsapp/Whatsapp.jsx
+++ client/src/components/Whatsapp/Whatsapp.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable-next-line no-unused-vars */
+// 
 import React from "react";
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Whatsapp.css";
@@ -6,15 +6,17 @@ import "./Whatsapp.css";
 const Whatsapp = () => {
   return (
     <div className="whatsapp-container">
-      <div className="whatsapp_float sm:p-2 sm:mr-1 lg:p-2">
+      <div className="whatsapp_float  sm:p-2 sm:mr-1 lg:p-2">
         <a
           href="https://wa.me/9346023775"
           target="_blank"
           rel="noopener noreferrer"
         >
           <i className="fa fa-whatsapp whatsapp-icon mb-[10px]"></i>
-          <div className="whatsapp-text flex justify-center xl:pb-0 xl:text-base xl:gap-1 xl:mt-8 
-          sm:pb-10  sm:text-lg lg:pb-10">
+          <div
+            className="whatsapp-text flex justify-center xl:pb-0 xl:text-base xl:gap-1 xl:mt-8 
+          sm:pb-10  sm:text-lg lg:pb-10"
+          >
             <p>Chat</p>
             <p>with</p>
             <p>us</p>
@@ -26,5 +28,3 @@ const Whatsapp = () => {
 };
 
 export default Whatsapp;
-    
-   
