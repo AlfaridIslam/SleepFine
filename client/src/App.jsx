9327802bc4ac0f36c16b0aa0a7574bd6b7b6f9e9
P Venkatesh <patlavathvenkatesh2001@gmail.com>
2024-12-03 11:30:33 +0530

    03/12--dont update small change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import Spinner from "./components/Spinner/Spinner"; // Adjust path if needed
+import Spinner from "./components/spinner/Spinner"; // Adjust path if needed
 
 const App = () => {
   const [loading, setLoading] = React.useState(false);
